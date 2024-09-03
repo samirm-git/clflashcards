adf87454e4cd8294656958d9a5c35953685d5a97
Samir Mahmood <samirmalik7@hotmail.co.uk>
2024-09-03 11:41:20 +0100

    setting up Go and Cobra

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote/v4"
+)
+
+func hello() {
+	fmt.Println(quote.Hello())
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/samirm-git/clflashcards.git/cmd"
+
+func main() {
+	cmd.Execute()
+}
